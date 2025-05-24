0fdc878f774b869e94bac621809152d094e3c018
a7medal <ahmedalahmedbedi@gmail.com>
2025-05-24 20:42:03 +0000

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,30 @@
+const CACHE_NAME = 'my-cache-v1';
+const FILES_TO_CACHE = [
+  'caisse.html',
+  'gaza-telecom.html',
+  'home.html',
+  'index.html',
+  'logo.png',
+  'mahfoud.html',
+  'pay.html'
+];
+
+// تثبيت Service Worker وتخزين الملفات في الكاش
 self.addEventListener('install', (e) => {
   console.log('Service Worker: Installed');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 });
 
+// جلب الملفات من الكاش عند الطلب
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      // إذا وُجد الملف في الكاش، يتم إرجاعه، وإلا يتم تحميله من الإنترنت
+      return response || fetch(e.request);
+    })
+  );
 });
